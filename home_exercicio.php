212b4b1fb11b631f46e769e5bb2a175610eab555
Nick146Br <nicol.gomes146@gmail.com>
2022-08-23 01:09:39 -0300

    Alteracao contest

--- home_exercicio.php
+++ home_exercicio.php
@@ -32,7 +32,7 @@
     onclick="location.href = 'home.php';"> HOME </button>
 
 
-	<div class="container" id="cad_submissao" style="min-width: 400px; min-height: 470px; height: auto; overflow: auto;">
+	<div class="container" id="cad_submissao" style="min-width: 400px; min-height: 520px; height: auto; overflow: auto;">
 		<p class="login-text" style="font-size: 2rem; font-weight: 800;">Sistemas LPC</p>
         
         O que deseja Fazer?<br>
